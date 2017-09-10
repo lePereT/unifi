446008f36822cc0f4c3e666ef57e754da7d77efe
lePereT <richardthanki@gmail.com>
2017-09-10 04:25:09 +0100

    using the full stat/sites schema

--- sites.go
+++ sites.go
@@ -3,14 +3,30 @@ package unifi
 // A Site is a physical location with UniFi devices managed by a UniFi
 // Controller.
 type Site struct {
-	ID          string `json:"_id"`
-	Description string `json:"desc"`
-	Name        string `json:"name"`
-	NumAPs      int    `json:"health,num_ap"`
-	NumUsers    int    `json:"health,num_user"`
-	NumGuests   int    `json:"health,num_guest"`
-	NumStations int
-	Role        string `json:"role"`
+	ID           string `json:"_id"`
+	AttrHiddenID string `json:"attr_hidden_id,omitempty"`
+	AttrNoDelete bool   `json:"attr_no_delete,omitempty"`
+	Desc         string `json:"desc"`
+	Health       []struct {
+		NumAdopted      int    `json:"num_adopted,omitempty"`
+		NumAp           int    `json:"num_ap,omitempty"`
+		NumDisabled     int    `json:"num_disabled,omitempty"`
+		NumDisconnected int    `json:"num_disconnected,omitempty"`
+		NumGuest        int    `json:"num_guest,omitempty"`
+		NumPending      int    `json:"num_pending,omitempty"`
+		NumUser         int    `json:"num_user,omitempty"`
+		RxBytesR        int    `json:"rx_bytes-r,omitempty"`
+		Status          string `json:"status"`
+		Subsystem       string `json:"subsystem"`
+		TxBytesR        int    `json:"tx_bytes-r,omitempty"`
+		NumGw           int    `json:"num_gw,omitempty"`
+		NumSw           int    `json:"num_sw,omitempty"`
+	} `json:"health"`
+	Name             string  `json:"name"`
+	NumNewAlarms     int     `json:"num_new_alarms"`
+	LocationAccuracy int     `json:"location_accuracy,omitempty"`
+	LocationLat      float64 `json:"location_lat,omitempty"`
+	LocationLng      float64 `json:"location_lng,omitempty"`
 }
 
 // Sites returns all of the Sites managed by a UniFi Controller.
@@ -29,10 +45,6 @@ func (c *Client) Sites() ([]*Site, error) {
 	}
 
 	_, err = c.do(req, &v)
-
-	for i, s := range v.Sites {
-		v.Sites[i].NumStations = s.NumGuests + s.NumUsers
-	}
 
 	return v.Sites, err
 }
