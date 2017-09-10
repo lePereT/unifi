920963267f13f1b6c801faba55a165a8eea6d1bd
lePereT <richardthanki@gmail.com>
2017-09-10 23:35:25 +0100

    using the full stat/sites schema

--- sites.go
+++ sites.go
@@ -1,37 +1,5 @@
 package unifi
 
-// A Site is a physical location with UniFi devices managed by a UniFi
-// Controller.
-
-type Site struct {
-	ID               string   `json:"_id"`
-	AttrHiddenID     string   `json:"attr_hidden_id,omitempty"`
-	AttrNoDelete     bool     `json:"attr_no_delete,omitempty"`
-	Desc             string   `json:"desc"`
-	Health           []health `json:"health"`
-	Name             string   `json:"name"`
-	NumNewAlarms     int      `json:"num_new_alarms"`
-	LocationAccuracy float64  `json:"location_accuracy,omitempty"`
-	LocationLat      float64  `json:"location_lat,omitempty"`
-	LocationLng      float64  `json:"location_lng,omitempty"`
-}
-
-type health struct {
-	NumAdopted      int    `json:"num_adopted,omitempty"`
-	NumAp           int    `json:"num_ap,omitempty"`
-	NumDisabled     int    `json:"num_disabled,omitempty"`
-	NumDisconnected int    `json:"num_disconnected,omitempty"`
-	NumGuest        int    `json:"num_guest,omitempty"`
-	NumPending      int    `json:"num_pending,omitempty"`
-	NumUser         int    `json:"num_user,omitempty"`
-	RxBytesR        int    `json:"rx_bytes-r,omitempty"`
-	Status          string `json:"status"`
-	Subsystem       string `json:"subsystem"`
-	TxBytesR        int    `json:"tx_bytes-r,omitempty"`
-	NumGw           int    `json:"num_gw,omitempty"`
-	NumSw           int    `json:"num_sw,omitempty"`
-}
-
 // Sites returns all of the Sites managed by a UniFi Controller.
 func (c *Client) Sites() ([]*Site, error) {
 	var v struct {
@@ -49,5 +17,49 @@ func (c *Client) Sites() ([]*Site, error) {
 
 	_, err = c.do(req, &v)
 
+	req, err := c.newRequest(
+		"GET",
+		"/api/self/sites",
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = c.do(req, &v)
+
 	return v.Sites, err
+}
+
+// A Site is a physical location with UniFi devices managed by a UniFi
+// Controller.
+type Site struct {
+	ID               string    `json:"_id"`
+	AttrHiddenID     string    `json:"attr_hidden_id"`
+	AttrNoDelete     bool      `json:"attr_no_delete"`
+	Desc             string    `json:"desc"`
+	Health           []*Health `json:"health"`
+	Name             string    `json:"name"`
+	NumNewAlarms     int       `json:"num_new_alarms"`
+	LocationAccuracy float64   `json:"location_accuracy"`
+	LocationLat      float64   `json:"location_lat"`
+	LocationLng      float64   `json:"location_lng"`
+	Role             string    `json:"role"`
+}
+
+// Health is a thing
+type Health struct {
+	NumAdopted      int    `json:"num_adopted"`
+	NumAp           int    `json:"num_ap"`
+	NumDisabled     int    `json:"num_disabled"`
+	NumDisconnected int    `json:"num_disconnected"`
+	NumGuest        int    `json:"num_guest"`
+	NumPending      int    `json:"num_pending"`
+	NumUser         int    `json:"num_user"`
+	RxBytesR        int    `json:"rx_bytes-r"`
+	Status          string `json:"status"`
+	Subsystem       string `json:"subsystem"`
+	TxBytesR        int    `json:"tx_bytes-r"`
+	NumGw           int    `json:"num_gw"`
+	NumSw           int    `json:"num_sw"`
 }
